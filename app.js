86507eb45e7d912ad9770682752a64a4ecc114b4
wasiqsiddiqui <wasiqsiddiqui@yahoo.com>
2019-04-14 12:02:11 -0500

    :Extra files for heart rate

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ var userId = url.split("#")[1].split("=")[2].split("&")[0];
 console.log(access_token);
 console.log(userId);
 
-httpss://api.fitbit.com/1/user/5YW8G8/activities/heart/date/today/1w.json
-
-
+/*
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'httpss://api.fitbit.com/1/user/'+ userId +'/activities/heart/date/today/1m.json');
+xhr.open('GET', 'httpss://api.fitbit.com/1/user/5YW8G8/activities/heart/date/today/1w.json');
 xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
 xhr.onload = function() {
    if (xhr.status === 200) {
@@ -22,5 +20,5 @@ xhr.onload = function() {
       document.write(xhr.responseText);
 	   }
 	};
-	xhr.send()
-	
+xhr.send();
+*/
